325970928f371363bf3408747f74dba69b35634c
Qfusi <Jonathan.Isaksson93@gmail.com>
2023-12-10 19:14:07 +0100

    add day 9

---
+++ 2023/day-9/Program.cs
@@ -0,0 +1,10 @@
+﻿using day_9;
+
+var input = File.ReadAllLines("input.txt");
+
+// var solution = new Solution(input);
+var result = Solution.PartOne(input);
+var result2 = Solution.PartTwo(input);
+
+Console.WriteLine($"part one: {result}");
+Console.WriteLine($"part two: {result2}");
---
+++ 2023/day-9/Solution.cs
@@ -0,0 +1,19 @@
+﻿
+namespace day_9;
+
+public static class Solution
+{
+    internal static int PartOne(string[] input) => input.Sum(line => GetNextNumberFromRange(line.Split().Select(int.Parse).ToArray()));
+    internal static int PartTwo(string[] input) => input.Sum(line => GetNextNumberFromRange(line.Split().Select(int.Parse).ToArray(), true));
+
+    private static int GetNextNumberFromRange(IReadOnlyList<int> numbers, bool partTwo = false)
+    {
+        if (numbers.All(x => x == 0))
+            return 0;
+
+        var differences = numbers.Skip(1).Select((x, i) => x - numbers[i]).ToArray();
+        return partTwo
+        ? numbers[0] - GetNextNumberFromRange(differences, partTwo)
+        : numbers[^1] + GetNextNumberFromRange(differences);
+    }
+}
